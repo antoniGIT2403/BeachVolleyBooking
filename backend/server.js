8131cb281706483238b833bbe443d67cefaff2a9
Antoni Malki <antonimalki1990@gmail.com>
2025-06-23 12:16:48 +0200

    last bycrypt version

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 const rateLimit = require("express-rate-limit");
 const { authMiddleware, isAdmin } = require("./middlewares/auth");
 require('dotenv').config();
